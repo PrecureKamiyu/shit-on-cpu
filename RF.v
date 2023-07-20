4bc1dad8219cc883ad5c544808029ce1a19a4805
PrecureKamiyu <92973191+PrecureKamiyu@users.noreply.github.com>
2023-07-20 17:18:58 +0800

    some verification to ID and RF
    some verification

--- RF.v
+++ RF.v
@@ -7,21 +7,16 @@ module RF (
     input wire [31:0] wD,
     input wire we,
     
-    output reg [31:0] rD1,
-    output reg [31:0] rD2
+    output wire [31:0] rD1,
+    output wire [31:0] rD2
 );
-    reg [31:0] array [0:31];
+    reg [31:0] array [31:0];
     
-    always @(*) begin
-        if (we == 0) begin 
-            rD1 = array[rR1];
-            rD2 = array[rR2];
-        end else ;
-    end
+    assign rD1 = (rR1 == 5'b0)? 32'b0 : array[rR1];
+	assign rD2 = (rR2 == 5'b0)? 32'b0 : array[rR2];
     
     always @(posedge clk) begin
-        if (we) begin
-            array[wR] <= wD;
-        end else ;
+        if(we && (wR != 5'b0)) 
+            array [wR] <= wD;
     end
 endmodule
