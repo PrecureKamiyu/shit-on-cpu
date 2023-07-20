e8ddb2354a2bf792ac2713054bd6eb91dce1df2a
PrecureKamiyu <92973191+PrecureKamiyu@users.noreply.github.com>
2023-07-20 16:40:37 +0800

    Update ALU.v
    make C reg

--- ALU.v
+++ ALU.v
@@ -4,26 +4,22 @@ module ALU (
     input wire [1:0] op,
     input wire [31:0] A,
     input wire [31:0] B,
-    output wire [31:0] C
+    output reg [31:0] C
 );
-
-reg[31:0] ans;
 
 always @(*) begin
     case(op)
-        `ALU_ADD : ans = A + B;
-        `ALU_SUB : ans = A - B;
-        `ALU_AND : ans = A & B;
-        `ALU_OR  : ans = A | B;
-        `ALU_XOR : ans = A ^ B;
-        `ALU_SLL : ans = A << B[4:0];
-        `ALU_SRL : ans = A >> B[4:0];
-        `ALU_SRA : ans = ($signed(A)) >>> B[4:0];
+        `ALU_ADD : C = A + B;
+        `ALU_SUB : C = A - B;
+        `ALU_AND : C = A & B;
+        `ALU_OR  : C = A | B;
+        `ALU_XOR : C = A ^ B;
+        `ALU_SLL : C = A << B[4:0];
+        `ALU_SRL : C = A >> B[4:0];
+        `ALU_SRA : C = ($signed(A)) >>> B[4:0];
         // this one!
-        default  : ans = 32'b0;
+        default  : C = 32'b0;
     endcase
 end
-
-assign C = ans;
 
 endmodule
